cd4546a7c0f22724526b46e760f55da8816f0b7f
msscompany1 <m.soboh@bau.edu.lb>
2026-08-13 11:47:38 +0300

    Use Rowad header and footer

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,10 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Route, Routes, Outlet } from 'react-router-dom';
 
-import Header from '@edx/frontend-component-header';
-import { FooterSlot } from '@edx/frontend-component-footer';
+import {
+  RowadFooter,
+  RowadHeader,
+} from '@edx/frontend-component-header';
 
 import configureStore from './data/configureStore';
 import AccountSettingsPage, { NotFoundPage } from './account-settings';
@@ -31,11 +33,11 @@ subscribe(APP_READY, () => {
         <Routes>
           <Route element={(
             <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-              <Header />
+              <RowadHeader />
               <main className="flex-grow-1" id="main">
                 <Outlet />
               </main>
-              <FooterSlot />
+              <RowadFooter />
             </div>
         )}
           >
